5fe355fb967d055b810d515eff1295be510f0665
Michael <ingyedu@gmail.com>
2023-05-27 14:41:14 +0000

    react project1

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./brand.css";
+import { google, slack, artlassian, dropbox, shopify } from "./imports.js";
+const Brand = () => {
+  return <div>Brand</div>;
+};
+
+export default Brand;
